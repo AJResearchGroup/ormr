99f0febd35ef15f8ed9845e30854520108cdd1b7
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-19 14:03:04 +0100

    Fix test

--- tests/testthat/test-does_conda_env_exists.R
+++ tests/testthat/test-does_conda_env_exists.R
@@ -4,10 +4,9 @@ test_that("use", {
   ormr_folder_name <- tempfile()
   expect_false(does_conda_env_exists(ormr_folder_name = ormr_folder_name))
 
-  install_miniconda(ormr_folder_name = ormr_folder_name)
+  create_conda_env(ormr_folder_name = ormr_folder_name)
 
-  expect_true(does_conda_env_exists(ormr_folder_name = ormr_folder_name))
-  gcaer_report(ormr_folder_name = ormr_folder_name)
+  ormr_report(ormr_folder_name = ormr_folder_name)
 
   unlink(ormr_folder_name, recursive = TRUE)
 })
