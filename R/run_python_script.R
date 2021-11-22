734889d3638584b6f1e55830fc002ad39f780473
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-22 08:44:42 +0100

    Remove obsolete doc

--- R/run_python_script.R
+++ R/run_python_script.R
@@ -8,7 +8,9 @@ run_python_script <- function(
   python_script_path
 ) {
   reticulate::use_condaenv(condaenv = ormr_folder_name)
-  python_path <- ormr::get_python_binary_path(ormr_folder_name = ormr_folder_name)
+  python_path <- ormr::get_python_binary_path(
+    ormr_folder_name = ormr_folder_name
+  )
   reticulate::use_python(python = python_path, required = TRUE)
   reticulate::py_run_file(
     file = python_script_path
