9def907fad414d24b9df901feb373dbe928a16ac
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-22 09:11:54 +0100

    Increase codecov

--- tests/testthat/test-ormr_report.R
+++ tests/testthat/test-ormr_report.R
@@ -1,3 +1,17 @@
-test_that("use", {
+test_that("use, nothing installed", {
   ormr_report(ormr_folder_name = tempfile())
 })
+
+test_that("minimal use", {
+  expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
+  if (!plinkr::is_on_ci()) return()
+
+  ormr_folder_name <- tempfile()
+
+  create_conda_env(ormr_folder_name = ormr_folder_name)
+
+  ormr_report(ormr_folder_name = tempfile())
+
+  unlink(ormr_folder_name, recursive = TRUE)
+})
+
