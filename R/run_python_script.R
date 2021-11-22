2a89b4b9b0f51ee0893d8153429fadc08b1b4a1d
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-22 12:27:59 +0100

    'run_python_script' returns the output of the script

--- R/run_python_script.R
+++ R/run_python_script.R
@@ -1,19 +1,21 @@
 #' Run a Python script
 #' @inheritParams default_params_doc
-#' @return Nothing
+#' @return The output of the Python script
 #' @author Richèl J.C. Bilderbeek
 #' @export
 run_python_script <- function(
   ormr_folder_name,
-  python_script_path
+  python_script_path,
+  args = c()
 ) {
   reticulate::use_condaenv(condaenv = ormr_folder_name)
   python_path <- ormr::get_python_binary_path(
     ormr_folder_name = ormr_folder_name
   )
   reticulate::use_python(python = python_path, required = TRUE)
-  reticulate::py_run_file(
-    file = python_script_path
+  reticulate::py_capture_output(
+    reticulate::py_run_file(
+      file = python_script_path
+    )
   )
-  invisible(ormr_folder_name)
 }
--- tests/testthat/test-run_python_script.R
+++ tests/testthat/test-run_python_script.R
@@ -1,19 +1,52 @@
-test_that("use", {
+test_that("reprex for reiculate issue", {
+  skip("reprex")
+  expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
+  if (!plinkr::is_on_ci()) return()
+
+  # Create and activate a Conda environment
+  env_folder <- file.path(tempfile(), "reprex_env")
+  python_path <- reticulate::conda_create(
+    envname = env_folder
+  )
+  reticulate::use_condaenv(condaenv = env_folder)
+  reticulate::use_python(python = python_path, required = TRUE)
+
+  # Create a Hello World Python script
+  python_script_path <- tempfile(fileext = ".py")
+  writeLines(text = "print('Hello world')", con = python_script_path)
+
+  # Run the script
+  output <- reticulate::py_capture_output(
+    output <- reticulate::py_run_file(
+      file = python_script_path
+    )
+  )
+  # Confirm the output is as expected
+  expect_equal(
+    output,
+    "Hello world\n\n"
+  )
+})
+
+
+test_that("produces output", {
   expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
   if (!plinkr::is_on_ci()) return()
 
   ormr_folder_name <- tempfile()
   create_conda_env(ormr_folder_name = ormr_folder_name)
-
   python_script_path <- system.file(
     "extdata", "hello_world.py", package = "ormr"
   )
   expect_true(file.exists(python_script_path))
 
-  run_python_script(
+  output <- run_python_script(
     ormr_folder_name = ormr_folder_name,
     python_script_path = python_script_path
   )
-
+  expect_equal(
+    output,
+    "Hello world\n\n"
+  )
   unlink(ormr_folder_name, recursive = TRUE)
 })
