33f88df9a0086f8ac57be9ab08f1ddbcd893da44
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-23 15:02:24 +0100

    Fix test

--- tests/testthat/test-install_python_packages.R
+++ tests/testthat/test-install_python_packages.R
@@ -1,26 +1,23 @@
 test_that("install scipy", {
-  expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
-  if (!plinkr::is_on_ci()) return()
-
   package_names <- "scipy"
   ormr_folder_name <- create_default_conda_env()
 
   # Package may already be installed
   is_python_package_installed(
     ormr_folder_name = ormr_folder_name,
-    package_names = package_names
+    package_name = package_names
   )
 
   expect_silent(
     install_python_packages(
       ormr_folder_name = ormr_folder_name,
-      package_namess = package_names
+      package_names = package_names
     )
   )
   expect_true(
     is_python_package_installed(
       ormr_folder_name = ormr_folder_name,
-      package_names = package_names
+      package_name = package_names
     )
   )
 })
