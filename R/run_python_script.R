e5553be2bff7551e8fa1029f71b95fa89b8e73db
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-24 12:21:40 +0100

    Remove dead code from 'run_python_script'

--- R/run_python_script.R
+++ R/run_python_script.R
@@ -12,19 +12,10 @@ run_python_script <- function(
     ormr_folder_name = ormr_folder_name
   )
   reticulate::use_python(python = python_path, required = TRUE)
-
-  if (1 + 1 == 2) {
-    output <- reticulate::py_capture_output(
-      reticulate::py_run_file(
-        file = python_script_path
-      )
+  output <- reticulate::py_capture_output(
+    reticulate::py_run_file(
+      file = python_script_path
     )
-  } else {
-    output <- reticulate::py_capture_output(
-      reticulate::source_python(
-        file = python_script_path
-      )
-    )
-  }
+  )
   output
 }
