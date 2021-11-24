3f8cf9b1bfa6892d50c6c640220f1ffd309ea18a
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-24 13:01:06 +0100

    Verify tensorflow is not installed

--- tests/testthat/test-is_python_package_with_version_installed.R
+++ tests/testthat/test-is_python_package_with_version_installed.R
@@ -1,4 +1,17 @@
-test_that("use", {
+test_that("use, tensorflow is not installed", {
+  ormr_folder_name <- create_default_conda_env()
+  t <- get_installed_python_packages(ormr_folder_name = ormr_folder_name)
+  expect_equal(sum(t$package == "tensorflow"), 0)
+  expect_false(
+    is_python_package_with_version_installed(
+      ormr_folder_name = ormr_folder_name,
+      package_name = "tensorflow",
+      package_version = "==1.0.0"
+    )
+  )
+})
+
+test_that("use, check versions", {
   ormr_folder_name <- create_default_conda_env()
   t <- get_installed_python_packages(ormr_folder_name = ormr_folder_name)
 
