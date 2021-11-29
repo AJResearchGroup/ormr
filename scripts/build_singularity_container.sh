d14539d724a5b34102ea4a40c5976f6e26b50766
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-29 09:02:33 +0100

    Use regular Singularity container

--- scripts/build_singularity_container.sh
+++ scripts/build_singularity_container.sh
@@ -10,9 +10,8 @@
 #
 #
 
-#sudo -E singularity --quiet build ormr.sif Singularity
+sudo -E singularity --quiet build ormr.sif Singularity
 #sudo -E singularity --quiet build --sandbox ormr.sif Singularity
-
-sudo -E singularity --quiet build --writeable ormr.sif Singularity
+#sudo -E singularity --quiet build --writeable ormr.sif Singularity
 
 
