536a06206c97cb63fb7ec0e4ce3638c83cf9c195
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-30 15:15:55 +0100

    Simplify script

--- scripts/demo_container.R
+++ scripts/demo_container.R
@@ -1,12 +1,13 @@
 library(ormr)
 
-# Remove these in the future, make 'ormr_report' more eager
-#reticulate::use_condaenv(condaenv = "/opt/ormr")
-#reticulate::use_python(python = reticulate:::python_binary_path("/opt/ormr"), required = TRUE)
-
-ormr_report(ormr_folder_name = "/opt/ormr", verbose = TRUE)
+ormr_report(ormr_folder_name = "/opt/ormr")
 
 python_script_path <- system.file("extdata", "scipy_example.py", package = "ormr")
+
+ormr::run_python_script(
+  ormr_folder_name = "/opt/ormr",
+  python_script_path = python_script_path
+)
 
 if (1 == 2) {
   python_script_path <- "scipy_example.py"
@@ -16,11 +17,3 @@ if (1 == 2) {
   )
   reticulate::py_run_file(file = python_script_path)
 }
-
-
-ormr::run_python_script(
-  ormr_folder_name = "/opt/ormr",
-  python_script_path = python_script_path,
-  verbose = TRUE
-)
-
