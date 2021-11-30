9a592d1141d4a00bc5bf3e081897e909ef9283a0
richelbilderbeek <richel@richelbilderbeek.nl>
2021-11-30 11:06:30 +0100

    Eager loading

--- tests/testthat/test-get_installed_python_packages.R
+++ tests/testthat/test-get_installed_python_packages.R
@@ -1,23 +1,12 @@
 test_that("minimal use", {
-  expect_silent(
-    get_installed_python_packages(
-      ormr_folder_name = create_default_conda_env()
-    )
+  expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
+  # No expect_silent, as may produce messages
+  get_installed_python_packages(
+    ormr_folder_name = create_default_conda_env()
   )
 })
 
-test_that("no env", {
-  ormr_folder_name <- tempfile()
-
-  expect_error(
-    get_installed_python_packages(ormr_folder_name = ormr_folder_name),
-    "failed to discover Python binary associated with path"
-  )
-  expect_false(dir.exists(ormr_folder_name))
-
-  skip("Now now")
-  expect_error(
-    get_installed_python_packages(ormr_folder_name = ormr_folder_name),
-    "Tip: run 'ormr::create_conda_env"
-  )
+test_that("eager", {
+  # Creates a Conda env if needed
+  get_installed_python_packages(ormr_folder_name = tempfile())
 })
