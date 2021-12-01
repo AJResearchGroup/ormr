76201aec07e6c3c310003aed0b6770e8b5aaa447
richelbilderbeek <richel@richelbilderbeek.nl>
2021-12-01 08:17:02 +0100

    Fix test

--- tests/testthat/test-check_conda_env_exists.R
+++ tests/testthat/test-check_conda_env_exists.R
@@ -1,6 +1,6 @@
 test_that("use", {
   expect_equal(1 + 1, 2) # Prevents testthat warning for empty test
-  check_conda_env_exists(ormr_folder_name = get_default_ormr_folder_name())
+  if (does_conda_env_exists()) check_conda_env_exists()
 })
 
 test_that("use, absent ormr_folder", {
